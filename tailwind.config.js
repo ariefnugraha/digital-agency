39db9e320eb478a81675f9b8ba8974574fb5be7c
arief <muhammadariefnugraha77@gmail.com>
2022-07-26 14:47:18 +0700

    buat custom utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'green-1': '#016170',
+        'black-1': '#020E10',
+        'yellow-1': '#FFC42C',
+        'green-2': '#00E0C6',
+        'white-2': '#EBE2D9',
+        'white-1': '#FFFFFC'
+      },
+      padding: {
+        '3.75': '3.75rem'
+      }
+    },
   },
   plugins: [],
 }
