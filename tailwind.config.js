047408f0e270973019a90ae93382ee6b103938e5
arief <muhammadariefnugraha77@gmail.com>
2022-08-23 12:30:40 +0700

    tambahan custom utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,23 @@ module.exports = {
       },
       padding: {
         '3.75': '3.75rem'
+      },
+      fontFamily: {
+        'wagon-bold': ['Wagon-Bold', 'serif'],
+        'wagon-extra-light': ['Wagon-ExtraLight', 'serif'],
+        'nunito-sans': ['Nunito Sans', 'sans-serif']
+      },
+      fontSize: {
+        '2x': ['2rem', '48px']
+      },
+      height: {
+        '42rem': '42rem',
+        '40rem': '40rem'
+      },
+      lineHeight: {
+        '48px': '48px',
+        '72px': '72px',
+        '98px': '98px'
       }
     },
   },
